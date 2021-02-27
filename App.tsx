010139911535896337b4a6edb8e8006a0df990ce
Richard Antao <richardmantao@gmail.com>
2021-02-27 18:23:51 -0500

    Created App Navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { enableScreens } from "react-native-screens";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { LoadAssets, StyleGuide, cards } from "./src/components";
+import Examples, { examples } from "./src/Examples";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import ClockValuesAndIdentities from "./src/animations/ClockValuesAndIdentities";
+
+import { Lessons } from "./src/components/Routes";
+
+enableScreens();
+
+const fonts = {
+	"SFProText-Bold": require("./assets/fonts/SF-Pro-Text-Bold.otf"),
+	"SFProText-Semibold": require("./assets/fonts/SF-Pro-Text-Semibold.otf"),
+	"SFProText-Regular": require("./assets/fonts/SF-Pro-Text-Regular.otf"),
+};
+
+const assets = [
+	...examples.map((example) => example.source),
+	...cards.map((card) => card.source),
+	// ...profiles.map((profile) => profile.profile),
+	// ...pictures,
+];
+
+const { Navigator, Screen } = createStackNavigator<Lessons>();
+
+const AppNavigator = () =>
+	<Navigator
+		screenOptions={{
+			headerStyle: {
+				backgroundColor: StyleGuide.palette.primary,
+				borderBottomWidth: 0,
+			},
+			headerTintColor: "white",
+		}}
+	>
+		<Screen
+			name="ClockValuesAndIdentities"
+			component={ClockValuesAndIdentities}
+			options={{
+				title: "Clock Values & Identities",
+			}}
+		/>
+	</Navigator>;
+
+const App = () =>
+	<LoadAssets {...{ fonts, assets }}>
+		<StatusBar barStyle="light-content" />
+		<AppNavigator />
+	</LoadAssets>;
+
+export default App;
